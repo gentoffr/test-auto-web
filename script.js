bf037c6979259fbe1a3830ec743c21f72b34c67d
gentoffr <frangento@hotmail.com>
2025-04-09 18:15:08 -0300

    Com1

---
+++ script.js
@@ -0,0 +1,20 @@
+function buscar(str){
+    fetch("C:\Users\frang\Desktop\Proy mini\src.json")
+    .then(res => res.json())
+    .then(data => {
+        let auto = data.find(a => a.nombre === str)
+        if(auto)
+        {
+            let nombre = document.createElement("n");
+            nombre.textContent = auto.nombre;
+            let ancho = auto.ancho;
+            let alto = auto.altura;
+            let largo = auto.longitud;
+            let imagenes = [...auto.imagenes];
+            contenedor.appendChild(nombre)
+        }
+    })
+}
+let contenedor = document.getElementById("contenedor");
+contenedor.innerHTML = "";
+let leerTexto = document.getElementById("auto").value.toLowerCase();
