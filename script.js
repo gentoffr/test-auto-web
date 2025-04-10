3fa264989b7895bc76255563aa3d5fc79f718b7b
gentoffr <frangento@hotmail.com>
2025-04-10 14:01:29 -0300

    json como array

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ function buscar(str){
         let auto = data.find(a => a.nombre === str)
         if(auto)
         {
-            let nombre = document.createElement("n");
+            let nombre = document.createElement("span");
             nombre.textContent = auto.nombre;
             let ancho = auto.ancho;
             let alto = auto.altura;
             let largo = auto.longitud;
             let imagenes = [...auto.imagenes];
-            contenedor.appendChild(nombre)
+            contenedor.appendChild(nombre);
         }
-
     })
 }
 let contenedor = document.getElementById("contenedor");
